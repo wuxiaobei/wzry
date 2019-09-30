21b1c5deb6b32c0a9de52f9a8a61ab59835368dc
吴晓贝 <wxbjxr@163.com>
2019-09-30 08:00:45 +0800

    fix upload url

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -50,7 +50,7 @@ module.exports = app => {
 
     app.post('/admin/api/upload', authMiddleware(), upload.single('file'), async (req, res) => {
         const file = req.file
-        file.url = `http://localhost:3000/uploads/${file.filename}`
+        file.url = `http://test.hebudan.xyz/${file.filename}`
         res.send(file)
     })
 
