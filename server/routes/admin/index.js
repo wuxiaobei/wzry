b3439c7d734385b4ea5dd615f4bfe1eb17296941
吴晓贝 <wxbjxr@163.com>
2019-11-06 16:27:26 +0800

    abc

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -6,14 +6,14 @@ module.exports = app => {
     const jwt = require('jsonwebtoken')
     const AdminUser = require('../../models/AdminUser')
     const assert = require('http-assert')
-    
+
     //创建资源
-    router.post('/', async(req, res) => {
+    router.post('/', async (req, res) => {
         const model = await req.Model.create(req.body)
         res.send(model)
     })
     //更新资源
-    router.put('/:id', async(req, res) => {
+    router.put('/:id', async (req, res) => {
         const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
         res.send(model)
     })
@@ -22,17 +22,17 @@ module.exports = app => {
         const queryOptions = {}
         if (req.Model.modeName = 'Category') {
             queryOptions.populate = 'parent'
-        } 
+        }
         const items = await req.Model.find().setOptions(queryOptions).limit(100)
         res.send(items)
     })
     //资源详情
-    router.get('/:id', async(req, res) => {
+    router.get('/:id', async (req, res) => {
         const model = await req.Model.findById(req.params.id)
         res.send(model)
     })
     //删除资源
-    router.delete('/:id', async(req, res) => {
+    router.delete('/:id', async (req, res) => {
         const model = await req.Model.findByIdAndDelete(req.params.id, req.body)
         res.send({
             success: true
@@ -45,9 +45,20 @@ module.exports = app => {
 
     app.use('/admin/api/rest/:resource', authMiddleware(), resourceMiddleWare(), router)
 
-    const multer = require('multer') 
-    const upload = multer({ dest: __dirname + '/../../uploads'})
+    const multer = require('multer')
+    // const MAO = require('multer-aliyun-oss');
 
+    // const upload = multer({
+    //     storage: MAO({
+    //         config: {
+    //             region: 'oss-cn-hongkong',
+    //             accessKeyId: 'LTAI4FkTyZSnPfs5JBj8fiwx',
+    //             accessKeySecret: 'icXpbsYxKoo5bc6yr2wfmFlNar9DS8',
+    //             bucket: 'node-vue-moba1'
+    //         }
+    //     })
+    // })
+    const upload = multer({ dest: __dirname + '/../../uploads' })
     app.post('/admin/api/upload', authMiddleware(), upload.single('file'), async (req, res) => {
         const file = req.file
         file.url = `http://test.hebudan.xyz/${file.filename}`
